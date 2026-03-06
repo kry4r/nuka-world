0ff78d30b5609a1b53e02046a51952e66058ea5e
Nidhog <Nidhogxt@outlook.com>
2026-03-06 22:47:00 +0800

    refactor: remove legacy go and web stack

--- web/src/components/ClientProviders.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { I18nProvider } from "@/lib/i18n";
-import type { ReactNode } from "react";
-
-export default function ClientProviders({ children }: { children: ReactNode }) {
-  return <I18nProvider>{children}</I18nProvider>;
-}
